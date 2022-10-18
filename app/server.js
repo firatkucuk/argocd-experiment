ca36732f349da5214d62dd6da3a38dc64e7bac73
Fırat KÜÇÜK <firatkucuk@gmail.com>
2022-10-18 22:20:46 +0200

    initial commit

---
+++ app/server.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+// Create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); // Write a response to the client
+  res.end(); // End the response
+}).listen(8080); // Server listens on port 8080
