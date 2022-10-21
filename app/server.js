59076634333d206a911e60c2e934fba267b0ba8b
Fırat KÜÇÜK <firatkucuk@gmail.com>
2022-10-21 20:50:15 +0200

    v12

--- app/server.js
+++ app/server.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 // Create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World v11'); // Write a response to the client
+  res.write('Hello World v12'); // Write a response to the client
   res.end(); // End the response
 }).listen(8080); // Server listens on port 8080
